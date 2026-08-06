cde40999375c8a261d4ba69c890d06ece8453307
Matt McCormack <matcor@noc.ac.uk>
2026-08-06 09:56:30 +0100

    Minor fix to series select all boxes

--- src/components/MenuBar/CheckableLabel.tsx
+++ src/components/MenuBar/CheckableLabel.tsx
@@ -5,17 +5,21 @@ interface CheckableLabelProps {
   onChange: (val: boolean) => void
   children: ReactNode
   tooltip?: string
+  inputId?: string
   inputClass?: string
 }
 
 /**
  * Text label with accompanying checkbox.
  */
-export const CheckableLabel = ({ defaultChecked, onChange, children, tooltip, inputClass }: CheckableLabelProps) => (
+export const CheckableLabel = ({
+  defaultChecked, onChange, children, tooltip, inputId, inputClass
+}: CheckableLabelProps) => (
   <div>
     <label className='qcp-check-label' title={tooltip}>
       <input
         type='checkbox'
+        id={inputId}
         className={inputClass}
         onChange={e => onChange(e.target.checked)}
         defaultChecked={defaultChecked}
